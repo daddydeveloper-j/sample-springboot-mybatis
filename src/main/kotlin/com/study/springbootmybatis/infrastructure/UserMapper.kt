d734871e4dbc37b477b2c6b96233f5cab4f9632f
daddydeveloper <daddydeveloper@naver.com>
2024-02-25 23:58:45 +0900

    refactor: UserMapper.xml 을 사용하도록 변경
    - UserMapper.xml 을 사용하여 쿼리를 호출 하도록 변경

--- src/main/kotlin/com/study/springbootmybatis/infrastructure/UserMapper.kt
+++ src/main/kotlin/com/study/springbootmybatis/infrastructure/UserMapper.kt
@@ -2,10 +2,8 @@ package com.study.springbootmybatis.infrastructure
 
 import com.study.springbootmybatis.domain.User
 import org.apache.ibatis.annotations.Mapper
-import org.apache.ibatis.annotations.Select
 
 @Mapper
 interface UserMapper {
-    @Select("SELECT * FROM ACCOUNTS")
     fun findAll(): List<User>
 }
